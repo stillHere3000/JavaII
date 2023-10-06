1bc8dfb29abf4b90b8290e88552a33e5d64e72c0
Trevor Maliro <trevor.t.maliro@gmail.com>
2023-10-06 07:24:10 -0400

    commit

--- src/Triangle.java
+++ src/Triangle.java
@@ -12,18 +12,73 @@ In addition, the class should have the following constructors and methods:
     6. A method named toString() that returns a string description for the triangle. 
  */
 
+ import java.util.Scanner;
+
+
+
+
 public class Triangle {
 
     private double side1;
     private double side2;
     private double side3;
+    private GeometricObject gObj;
 
+    /*
+     * Write a test program that does the following tasks:
+    1. Prompts the user to enter three sides of the triangle. 
+    - The program creates a Triangle object with these sides. 
+    - Prompt the user to enter a color and a Boolean value to indicate whether the triangle is filled. 
+    - Use these two input values to set the data fields color and filled. 
+    2. Invoke the Triangle object’s getPerimeter() and toString() methods. 
+     */
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Please the three sides of a the Triangle");
+        Scanner userInput= new Scanner(System.in);
+        Triangle T= captureTriangleValues(userInput);  
+        T.gObj =    captureGeometricValues(userInput);
+        System.out.println(T.toString());
+        
+    }
+
+    public static Triangle captureTriangleValues(Scanner in ){
+        double sd1, sd2, sd3; sd1= sd2= sd3= 1.0;
+        for(int i = 0;  i < 3 ; i++){
+           switch(i){                
+                case 0:    sd1= in.nextDouble();       break;
+                case 1:    sd2= in.nextDouble();       break;
+                case 2:    sd3= in.nextDouble();       break;
+                default:                            break;
+           }
+        }
+        return (new Triangle(sd1, sd2, sd3)); 
+    }
+
+    public static GeometricObject captureGeometricValues(Scanner in ){
+        String color="white";  Boolean filled= false;  String tmpColor;
+        for(int i = 0;  i < 3 ; i++){
+           switch(i){                
+                case 0:    
+                    System.out.println("Please Enter the Color");
+                    tmpColor= in.next();
+                    color = ( tmpColor== "")?"white":tmpColor;
+                break;
+                case 1:    
+                    System.out.println("Is the Triangle Filled (Y)es or (N)o");
+                    int temp= Character.toUpperCase(in.next().charAt(0) );
+                    filled= (temp == 'Y' )?true:false;
+                break;
+                
+                default:                            break;
+           }
+        }
+        return (new GeometricObject(color, filled)); 
     }
 
     public Triangle(){
         this.side1= this.side2= this.side3= 1.0;
+        this.gObj= new GeometricObject();
+       
     }
 
     public Triangle(double side1, double side2, double side3){
@@ -59,7 +114,8 @@ public class Triangle {
     }
 
     public String toString(){
-        String toStr= "Side1 "+this.side1+" Side2 "+this.side2+" Side3 "+this.side3;
-         return toStr;
+        String toStr= "\nSide1 "+this.side1+" Side2 "+this.side2+" Side3 "+this.side3+"\n";
+        toStr= toStr + this.gObj.toString();
+        return toStr;
     }
 }
